736c9ef8286d4c54e39d63e40cfdb66f4fe0e10e
Mohit Chotalia <Mohit Chotalia@192.168.0.103>
2023-07-15 21:07:10 +0530

    first commit

---
+++ BootMS-Proj05-ConsumerApp-FeignClient/src/main/java/com/nt/rest/BillingServiceController.java
@@ -0,0 +1,35 @@
+//BillingServiceController.java
+package com.nt.rest;
+
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.nt.feign.IBillingServiceClient;
+
+@RestController()
+@RequestMapping("/billing")
+public class BillingServiceController {
+	@Autowired
+	private IBillingServiceClient clientComp;
+	
+	@GetMapping("/info")
+	public ResponseEntity<String> getFinalGeneratedBillAmount(){
+		double billAmt = new Random().nextDouble(10000.0);
+		//use client comp to get the producer MS service instance from the Eureka server
+		ResponseEntity<String> response = clientComp.getPaymentOptions();
+		//generate the final result 
+		String finalResult = "Final Bill Amount :: "+billAmt+" and the Payment Options are :: "+response.getBody();
+		
+		return new ResponseEntity<String>(finalResult, HttpStatus.OK);
+		
+	}//method
+
+}//class
